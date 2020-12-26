2612d955bb27567579afe1620a1a57dd8aa5ccc2
Thitiwas Nupan <>
2020-12-26 14:30:30 +0700

    create navbar

---
+++ src/Navbar.js
@@ -0,0 +1,10 @@
+// import react for create component
+import React from 'react';
+
+// component name is Navber
+function Navbar() {
+    return <header className="navbar">Facebook</header>
+}
+
+// export for use
+export default Navbar;
